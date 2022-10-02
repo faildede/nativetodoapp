a1d43b3af211944b3a65787a32d86d8dff63e0e2
faildede <justworkur@mail.ru>
2022-10-02 22:16:18 +0600

    new

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { AddTodo } from "./src/AddTodo";
+import { Navbar } from "./src/Navbar";
+import { Todo } from "./src/Todo"
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    const newTodo = {
+      id: Date.now().toString(),
+      title: title
+    }
+
+    // setTodos((prevTodos) => {
+    //   return[
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+
+    setTodos(prev => [
+      ...prev,
+      {
+      id: Date.now().toString(),
+      title: title
+      }
+    ])
+  }
+
+  const removeTodo = id => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title="Todo App" />
+      <View style={styles.container}>
+      <AddTodo onSubmit={addTodo} />
+      <FlatList
+      data={todos}
+      keyExtractor ={item => item.id.toString()}
+      renderItem ={({item}) => (<Todo todo={item} onRemove={removeTodo} />)}
+      />
+      </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    paddingHorizontal:30,
+    paddingVertical:20
+  }
+})
